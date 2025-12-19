fdd6591fdf4589a043f0aae5d7ef70e06734bfa6
Prashant Sharma <prashant2006sharma@gmail.com>
2025-12-19 06:14:44 +0530

    Add SwitchCase Statements

---
+++ Step1: Learn the basics/Lec 1: Things to Know in C++/SwitchCase/days.cpp
@@ -0,0 +1,40 @@
+/*
+Take the day no and print the corresponding day
+for 1 print Monday,
+for 2 print Tuesday and so on for 7 print Sunday.
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int day;
+    cout << "Enter 1-7 any number to print that day";
+    cin >> day;
+    switch (day)
+    {
+        case 1:
+            cout << "Monday \n";
+            break;
+        case 2:
+            cout << "Tuesday \n";
+            break;
+        case 3:
+            cout << "Wednesday \n";
+            break;
+        case 4:
+            cout << "Thrusday \n";
+            break;
+        case 5:
+            cout << "Friday \n";
+            break;
+        case 6:
+            cout << "Saturday \n";
+            break;
+        case 7:
+            cout << "Sunday \n";
+            break;
+        default:
+            cout << "Choose between 1-7 only";
+            break;
+    }
+}
