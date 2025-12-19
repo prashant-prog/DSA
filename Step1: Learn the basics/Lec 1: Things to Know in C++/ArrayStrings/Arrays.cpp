fe739c4b431a04f51408c2e127d18c63f01988ef
Prashant Sharma <prashant2006sharma@gmail.com>
2025-12-19 06:52:18 +0530

    Add Arrays

---
+++ Step1: Learn the basics/Lec 1: Things to Know in C++/ArrayStrings/Arrays.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+        cout << "\n Enter element "<<i+1 << "\n";
+        cin >> arr[i];
+    }
+    cout << "[ ";
+    for(int i = 0; i<5;i++){
+        cout << "\t"<< arr[i] << "\t";
+    }
+    cout << " ]";
+}
