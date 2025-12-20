f3d1d47906037a8c7f71a3cfbdcb8bfd0d7b7971
Prashant Sharma <prashant2006sharma@gmail.com>
2025-12-20 19:49:25 +0530

    Add While Loops

---
+++ Step1: Learn the basics/Lec 1: Things to Know in C++/WhileLoop/whileLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  	// while loop to print numbers from 1 to 5
+    int i = 0;
+    while (i < 5) {
+        cout << "Prashant" << endl;
+        i++;
+    }
+
+    return 0;
+}
